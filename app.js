4b669521f73f67cef822b3a7b358fc76bfda8406
rinlama <rinjinlama83@gmail.com>
2020-09-19 21:07:17 -0400

    add node

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const port = 4000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+//var upload = multer({ dest: "uploads/" });
+
+var upload = multer({ storage: storage });
+
+app.post("/file", upload.single("file"), function (req, res, next) {
+  const file = req.file;
+  if (file) {
+    res.json(req.file);
+  } else throw "error";
+});
+
+app.post("/multiplefiles", upload.array("files"), function (req, res, next) {
+  const files = req.files;
+  if (Array.isArray(files) && files.length > 0) {
+    res.json(req.files);
+  } else {
+    res.status(400);
+    throw new Error("No file");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
