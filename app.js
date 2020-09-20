47d95e5d630c8aeda536a9d7386f225674853de2
rinlama <rinjinlama83@gmail.com>
2020-09-19 23:31:59 -0400

    node multer

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const multer = require("multer");
 const cors = require("cors");
+const multer = require("multer");
 
 const port = 4000;
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+  origin: "http://localhost:4200",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Express App test");
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -16,27 +25,28 @@ const storage = multer.diskStorage({
   },
 });
 
-//var upload = multer({ dest: "uploads/" });
+const upload = multer({ storage });
 
-var upload = multer({ storage: storage });
-
-app.post("/file", upload.single("file"), function (req, res, next) {
+app.post("/file", upload.single("file"), (req, res) => {
   const file = req.file;
+
   if (file) {
-    res.json(req.file);
-  } else throw "error";
+    res.json(file);
+  } else {
+    throw new Error("File upload unsuccessful");
+  }
 });
 
-app.post("/multiplefiles", upload.array("files"), function (req, res, next) {
+app.post("/multifiles", upload.array("files"), (req, res) => {
   const files = req.files;
+
   if (Array.isArray(files) && files.length > 0) {
-    res.json(req.files);
+    res.json(files);
   } else {
-    res.status(400);
-    throw new Error("No file");
+    throw new Error("File upload unsuccessful");
   }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log("Express App is running on localhost :" + port);
 });
